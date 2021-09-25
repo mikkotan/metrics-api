e0f886b2cd2489f2f0201c93e64661224c0e6e5e
Mikko Tan <mikkoyunltd@gmail.com>
2021-09-25 17:30:21 +0800

    Add simple rest api for metrics

---
+++ app/controllers/api/v1/metrics_controller.rb
@@ -0,0 +1,46 @@
+module Api
+  module V1
+    class MetricsController < ApplicationController
+      before_action :find_metric, only: [:update, :destroy]
+
+      def index
+        metrics = Metric.all
+        render json: metrics, status: :ok
+      end
+
+      def create
+        metric = Metric.new(metric_params)
+
+        if metric.valid?
+          render json: metric, status: :created
+        else
+          render json: { error: metric.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @metric.update(metric_params)
+          render json: metric, status: :ok
+        else
+          render json: { error: metric.errors.full_message }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @metric.destroy
+        head :no_content
+      end
+
+      private
+
+      def metric_params
+        params.permit(:name)
+      end
+
+      def find_metric
+        @metric = Metric.find(params[:id])
+        render json: { error: 'Metric not found' }, status: :not_found unless metric.present?
+      end
+    end
+  end
+end
