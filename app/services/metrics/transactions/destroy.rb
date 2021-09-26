ad4e78973057342f7be7afb206c1550668334618
Mikko Tan <mikkoyunltd@gmail.com>
2021-09-26 23:04:24 +0800

    Fix destroy transaction metric and delete all attach values

--- app/services/metrics/transactions/destroy.rb
+++ app/services/metrics/transactions/destroy.rb
@@ -4,12 +4,19 @@ module Metrics
       include App::Transaction
 
       def initialize(deps = {})
+        @db = deps[:db] || ActiveRecord::Base
         @find_metric = deps[:find_metric] || Operations::FindById.new
       end
 
       def call(id)
         metric = yield @find_metric.call(id)
-        metric.destroy
+        values = metric.values
+
+        @db.transaction do
+          values.destroy_all if values.any?
+          metric.destroy
+        end
+
         Success(deleted_metric_id: id)
       end
     end
--- spec/services/metrics/transactions/destroy_spec.rb
+++ spec/services/metrics/transactions/destroy_spec.rb
@@ -5,14 +5,28 @@ RSpec.describe Metrics::Transactions::Destroy do
     let!(:metric) { create(:metric, name: 'First Metric') }
     let(:metric_id) { metric.id }
 
-    it 'deletes the metric record' do
-      result = subject.call(metric_id)
-      deleted_metric_id = result.success[:deleted_metric_id]
-      deleted_metric = Metric.where(id: deleted_metric_id).limit(1).first
+    context 'when there are no attached values to a metric' do
+      it 'deletes the metric record' do
+        result = subject.call(metric_id)
+        deleted_metric_id = result.success[:deleted_metric_id]
+        deleted_metric = Metric.where(id: deleted_metric_id).limit(1).first
+  
+        expect(result).to be_success
+        expect(deleted_metric_id).to eq metric_id
+        expect(deleted_metric).to eq nil
+      end
+    end
 
-      expect(result).to be_success
-      expect(deleted_metric_id).to eq metric_id
-      expect(deleted_metric).to eq nil
+    context 'when there are attached metric values record' do
+      let!(:values) { create_list(:metric_value, 5, metric: metric) }
+
+      it 'returns success and deletes all attached values' do
+        result = subject.call(metric_id)
+        deleted_values = MetricValue.where(metric_id: metric_id)
+
+        expect(result).to be_success
+        expect(deleted_values).to be_empty
+      end
     end
   end
 end
