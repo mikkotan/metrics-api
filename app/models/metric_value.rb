7602bb3963b1ebb4d11c3df50e2db355674560d9
Mikko Tan <mikkoyunltd@gmail.com>
2021-09-26 15:58:10 +0800

    Update model specs

--- app/models/metric_value.rb
+++ app/models/metric_value.rb
@@ -1,3 +1,4 @@
 class MetricValue < ApplicationRecord
   belongs_to :metric
+  validates_presence_of :value, :timestamp
 end
--- spec/models/metric_value_spec.rb
+++ spec/models/metric_value_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MetricValue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:metric) }
+  it { should validate_presence_of(:value) }
+  it { should validate_presence_of(:timestamp) }
 end
