040c94ba9456b54c64ba7588ebff0aa3ab92f0b7
Mikko Tan <mikkoyunltd@gmail.com>
2021-09-27 23:31:54 +0800

    Remove unnecessary matchers

--- spec/models/metric_value_spec.rb
+++ spec/models/metric_value_spec.rb
@@ -2,6 +2,4 @@ require 'rails_helper'
 
 RSpec.describe MetricValue, type: :model do
   it { should belong_to(:metric) }
-  it { should validate_presence_of(:value) }
-  it { should validate_presence_of(:timestamp) }
 end
